5bb5234eff1c3eedb36757e0d3ddced73f49446f
Dima2 <nat-gorina@yandex.ru>
2026-05-17 09:05:16 +0200

    Reset onboarding state version

--- src/main/resources/static/js/config.js
+++ src/main/resources/static/js/config.js
@@ -1,5 +1,5 @@
 export const CUSTOM_VALUE = "__custom__";
-export const APP_VERSION = "20260514-flow-lock";
+export const APP_VERSION = "20260517-reset-onboarding";
 
 export function resetStoredUiVersion() {
     if (localStorage.getItem("grindy.uiVersion") === APP_VERSION) {
